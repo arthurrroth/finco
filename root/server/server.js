ed0742e7b52cb45a35d5800e8584c15c397ca229
arthurrroth <127439987+arthurrroth@users.noreply.github.com>
2023-08-14 11:58:57 +0200

    add README.md

--- root/server/server.js
+++ root/server/server.js
@@ -4,5 +4,5 @@ const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log("Server is running on port ", PORT);
+  console.log(`Server is running faaaast on port ${PORT} 🏎️💨 `);
 });
