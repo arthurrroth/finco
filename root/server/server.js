e0dd5ec609e348c699a0b226f81a25d1486f3a7f
arthurrroth <127439987+arthurrroth@users.noreply.github.com>
2023-08-14 16:29:22 +0200

    Init

--- root/server/server.js
+++ root/server/server.js
@@ -1,7 +1,15 @@
 import express from "express";
-
+import cors from "cors";
+import "./config/mongoDB.js";
 const app = express();
-const PORT = 3000;
+app.use(cors());
+
+const PORT = 3322;
+
+app.get('/database', async (req, res) => {
+  res.send(200)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running faaaast on port ${PORT} 🏎️💨 `);
