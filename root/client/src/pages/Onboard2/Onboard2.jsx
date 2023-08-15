ca93c24ebe74b434d5d124de40badb37d6aa792f
pPaulaner690 <paula.krug2000@gmail.com>
2023-08-15 10:00:13 +0200

    onboarding without loading Animation

--- root/client/src/pages/Onboard2/Onboard2.jsx
+++ root/client/src/pages/Onboard2/Onboard2.jsx
@@ -1,14 +1,15 @@
 import "./Onboard2.css";
 import { NavLink } from "react-router-dom";
 import illustration from "../../icon/Illustration-gift.png"
+import arrow from "../../icon/Line.png"
 
 const Onboard2 = () => {
   return(
-    <main>
-      <img src={illustration} alt="Gifts and Card" />
-      <h2>Analyze yourspending</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt et.</p>
-      <NavLink to={"/signUp"}>Get Started</NavLink>
+    <main className="onboardMain">
+      <img className="onboardIllustration" src={illustration} alt="Gifts and Card" />
+      <h2 className="onboardH2">Analyze your <br/> spending</h2>
+      <p className="onboardText">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt et.</p>
+      <NavLink to={"/signUp"} className="blueBtn">Get Started	<img className="onboardArrow" src={arrow} alt="arrow" /></NavLink>
     </main>
   );
 };
