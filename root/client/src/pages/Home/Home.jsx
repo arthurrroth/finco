ab08461daaa9d0db0829d106aeca1a426ad0ab06
ChrisK2611 <christiankeil3@yahoo.de>
2023-08-17 09:50:50 +0200

    startet Home

--- root/client/src/pages/Home/Home.jsx
+++ root/client/src/pages/Home/Home.jsx
@@ -2,6 +2,9 @@ import "./Home.css";
 import Nav from "../../components/Nav/Nav.jsx";
 import { PageContext } from "../../context/context";
 import { useContext, useEffect } from "react";
+import CircleIcon from "../../icon/grayCircle.png"
+import LogoIcon from "../../icon/Logo-icon.png"
+import GroupIcon from "../../icon/Group-Icon.png"
 
 const Home = () => {
   const {page, setPage} = useContext(PageContext)
@@ -13,7 +16,25 @@ const Home = () => {
 
   return (
     <>
-    <h1>Home</h1>
+    <div className="home-top">
+      <h5>Welcome Back</h5>
+      <h2>Name</h2>
+      <img src={CircleIcon} alt="" />
+    </div>
+    <div className="creditcard">
+      <div className="">
+        <img src={LogoIcon} alt="" />
+        <h4>Credit Card</h4>
+        <p>**** 1289</p>
+          <div>
+            <img src={GroupIcon} alt="" />
+            <p>09/25</p>
+          </div>
+      </div>
+    </div>
+    <div className="Wallet">
+      <h3>Total Wallet</h3>
+    </div>
     <Nav page = {page}/>
     </>
   )
