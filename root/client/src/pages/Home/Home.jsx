5c3df193194adfe9c29af5a0dde3f6aec6e5734c
ChrisK2611 <christiankeil3@yahoo.de>
2023-08-21 11:00:18 +0200

    added spending-limit with edit

--- root/client/src/pages/Home/Home.jsx
+++ root/client/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import "./Home.css";
+
 // import methods
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
+
 // import components
 import Nav from "../../components/Nav/Nav.jsx";
 import IncomeExpense from "../../components/IncomeExpense/IncomeExpense";
 import Header from "../../components/Header/Header";
+
 // import context
 import {
   OpenBoxContext,
@@ -16,15 +19,28 @@ import {
 // import img
 import LogoIcon from "../../icon/Logo-icon.png";
 import GroupIcon from "../../icon/Group-Icon.png";
+import DangerIcon from "../../icon/icon-danger.svg";
+import ThreeDot from "../../icon/threedot-icon.svg";
 
 const Home = () => {
   const { selectedCard } = useContext(SelectedCardContext);
   const { page, setPage } = useContext(PageContext);
   const { setOpenBox } = useContext(OpenBoxContext);
 
+
   const [income, setIncome] = useState(0);
   const [expenses, setExpenses] = useState(0);
   const [transactions, setTransactions] = useState([]);
+  const [limit, setLimit] = useState(0);
+  const [editLimit, setEditLimit] = useState(false)
+
+  const handleEditLimit = () => {
+    setEditLimit(true);
+  };
+
+  const handleSaveLimit = () => {
+    setEditLimit(false);
+  };
 
   useEffect(() => {
     setPage("Home");
@@ -70,6 +86,35 @@ const Home = () => {
       <article className="income-expense">
         <IncomeExpense sortAmount={"income"} transaction={transactions} />
         <IncomeExpense sortAmount={"expense"} transaction={transactions} />
+      </article>
+      <article className="monthly-spending">
+        <div className="danger-icon">
+          <img src={DangerIcon} alt="" />
+        </div>
+        <div className="limit">
+          <p>Monthly spending limit</p>
+          {editLimit ? (
+            <input
+              type="number"
+              value={limit}
+              onChange={(e) => setLimit(e.target.value)}
+            />
+          ) : (
+            <h3>${limit}</h3>
+          )}
+        </div>
+        {editLimit ? (
+          <button className="save-button"
+          onClick={handleSaveLimit}>
+            Save</button>
+        ) : (
+          <img
+            src={ThreeDot}
+            alt=""
+            onClick={handleEditLimit}
+            className="edit-icon"
+          />
+        )}
       </article>
       <Nav page={page} />
     </>
