a1822dc596e651bd6c69fb501aa12566e47e42a1
Roman Parfentev <romanntev@gmail.com>
2023-08-28 04:01:40 +0200

    bugfix

--- root/server/config/index.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({
-  path: path.join(path.resolve(), "..", ".env"),
-});
-
-mongoose.connect(process.env.DB);
